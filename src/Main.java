9031c55d9ca4b07bdc9fd8f1a1f3d515521b0335
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2021-05-17 12:10:36 -0400

    printf com varias variaves

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
 		
 		Locale.setDefault(Locale.US);
 		
-		double x = 10.35784;
-		System.out.println(x);
-		System.out.printf("%.2f%n", x);
-		System.out.printf("%.4f%n", x);
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
 
 	}
 
