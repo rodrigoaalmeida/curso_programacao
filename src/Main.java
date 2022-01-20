8a043a5d7d0437436d23ef9c955ef563c8252b7e
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2022-01-20 08:52:32 -0400

    scanner com next com nextline

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,17 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		int x;
 		String s1, s2, s3;
 		
+		x = sc.nextInt();
+		sc.nextLine();
 		s1 = sc.nextLine();
 		s2 = sc.nextLine();
 		s3 = sc.nextLine();
 		
 		System.out.println("Dados Digitados:");
+		System.out.println(x);
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
