7d5ca9b30d5d18d51c1a332ae8a6099312af067e
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2022-01-21 07:24:24 -0400

    If else em cadeia

--- src/Main.java
+++ src/Main.java
@@ -8,29 +8,20 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double x = 3.0;
-		double y = 4.0;
-		double z = -5.0;
-		double A, B, C;
+		int hora;
 		
-		A = Math.sqrt(x);
-		B = Math.sqrt(y);
-		C = Math.sqrt(25.0);
-		System.out.println("Raiz quadrada de " + x + " = " + A);
-		System.out.println("Raiz quadrada de " + y + " = " + B);
-		System.out.println("Raiz quadrada de 25 = " + C);
+		System.out.println("Quantas horas?");
+		hora = sc.nextInt();
 		
-		A = Math.pow(x, y);
-		B = Math.pow(x, 2.0);
-		C = Math.pow(5.0, 2.0);
-		System.out.println(x + " elevado a " + y + " = " + A);
-		System.out.println(x + " elevado ao quadrado = " + B);
-		System.out.println("5 elevado ao quadrado = " + C);
-		
-		A = Math.abs(y);
-		B = Math.abs(z);
-		System.out.println("Valor absoluto de " + y + " = " + A);
-		System.out.println("Valor absoluto de " + z + " = " + B);
+		if (hora < 12) {
+			System.out.println("Bom dia");
+		} else {
+			if (hora < 18) {
+				System.out.println("Boa tarde");
+			} else {
+				System.out.println("Boa noite");
+			}
+		}
 		
 		
 		sc.close();
