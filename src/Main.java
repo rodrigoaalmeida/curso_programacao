30aeb9b93d45da1e9a476487e5ee1d7c96f094f4
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2022-01-21 09:30:11 -0400

    Switch case

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,40 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		int hora;
-		
-		System.out.println("Quantas horas?");
-		hora = sc.nextInt();
-		
-		if (hora < 12) {
-			System.out.println("Bom dia");
-		} else if (hora < 18) {
-			System.out.println("Boa tarde");
-		} else {
-			System.out.println("Boa noite");
+
+		int x = sc.nextInt();
+		String dia;
+		switch (x) {
+		case 1:
+			dia = "domingo";
+			break;
+		case 2:
+			dia = "segunda";
+			break;
+		case 3:
+			dia = "terca";
+			break;
+		case 4:
+			dia = "quarta";
+			break;
+		case 5:
+			dia = "quinta";
+			break;
+		case 6:
+			dia = "sexta";
+			break;
+		case 7:
+			dia = "sabado";
+			break;
+		default:
+			dia = "valor invalido";
+			break;
 		}
-		
-		
+		System.out.println("Dia da semana: " + dia);
+
 		sc.close();
 
 	}
