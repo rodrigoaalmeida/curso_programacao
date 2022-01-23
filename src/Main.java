2ec60e8a71e832a191c72fcb2a72bdf169ca2484
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2022-01-22 22:18:45 -0400

    Funcoes interecantes com String 01

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,31 @@
-import java.util.Locale;
-import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-
-		char resp;
+		String original = "abcde FGHIJ ABC abc DEFG ";
 		
-		do {
-			System.out.print("Digite a temperatura em Celsius: ");
-			double C = sc.nextDouble();
-			double F = 9.0 *C / 5.0 + 32.0;
-			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
-			System.out.print("Deseja repetir (s/n)? ");
-			resp = sc.next().charAt(0);
-		} while (resp != 'n');
-		sc.close();
+		String s01 = original.toLowerCase();
+		String s02 = original.toUpperCase();
+		String s03 = original.trim();//retira os espaços do inicio e fim da sentença.
+		String s04 = original.substring(2);
+		String s05 = original.substring(2, 9);
+		String s06 = original.replace('a', 'x');
+		String s07 = original.replace("abc", "xy");
+		int i = original.indexOf("bc");
+		int j = original.lastIndexOf("bc");
+		
+		System.out.println("Original: -" + original + "-");
+		System.out.println("toLowerCase: -" + s01 + "-");
+		System.out.println("toUpperCase: -" + s02 + "-");
+		System.out.println("trim: -" + s03 + "-");
+		System.out.println("substring(2): -" + s04 + "-");
+		System.out.println("substring(2, 9): -" + s05 + "-");
+		System.out.println("replace('a', 'x'): -" + s06 + "-");
+		System.out.println("replace(\"abc\", \"xy\"): -" + s07 + "-");
+		System.out.println("indexOf(\"bc\"): " + i);
+		System.out.println("lastIndexOf(\"bc\"): " + j);
 		
 
 	}
